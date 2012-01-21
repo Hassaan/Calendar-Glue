232c918a0b1655875f519319449690c432664b4e
Hassaan Aamir <hassaanaamir@hotmail.com>
2012-01-21 00:28:08 -0500

    Fixed the pop-up placement and map geocoding

--- js/background.js
+++ js/background.js
@@ -12,26 +12,44 @@ function onClickAction() {
  * Returns a handler which will open a new window when activated.
  */
 function getClickHandler() {
-  return function(info, tab) {
+     return function(info, tab) {
 
-    var url = 'event.html';
+          var url = 'event.html';
+         
+          //var screenWidth = $(window).width();
+          //var screenHeight = $(window).height();
+          var screenWidth = screen.height;
+          var screenHeight = screen.width;
+          var popupWidth = 550;
+          var popupHeight = 705;
+         
+          var leftPosition =  Math.floor(screenWidth/2);
+          var topPosition = Math.floor(screenHeight/2 - popupHeight);
 
-    // Create a new window to the add event page.
-    chrome.windows.create({ url: url, type: "popup"});
+          // Create a new window to the add event page.
+          chrome.windows.create({
+               url: url,
+               type: "popup", 
+               left: leftPosition,
+               top: topPosition,
+               width: popupWidth,
+               height: popupHeight,
+               focused: true
+          });
     
-  };
+     };
 };
 
 /**
  * Create a context menu which will only show up for text selections.
  */
 chrome.contextMenus.create({
-  "title" : "Add to Calendar",
-  "type" : "normal",
-  "contexts" : ["selection"],
-  "onclick" : getClickHandler()
+     "title" : "Add to Calendar",
+     "type" : "normal",
+     "contexts" : ["selection"],
+     "onclick" : getClickHandler()
 });
 
-  chrome.browserAction.onClicked.addListener(function(tab) {
-    onClickAction();
-  });
+chrome.browserAction.onClicked.addListener(function(tab) {
+     onClickAction();
+});
